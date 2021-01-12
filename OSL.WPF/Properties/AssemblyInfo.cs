6a60cc1a748dad234f312d305f45047f792001cb
Nicolas Mayeur <mail@mayeur.com>
2021-01-12 15:43:03 +0100

    Updated licence

--- OSL.WPF/Properties/AssemblyInfo.cs
+++ OSL.WPF/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿/* Copyright 2020 Nicolas Mayeur
+﻿/* Copyright 2021 Nicolas Mayeur
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -65,5 +65,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.2")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
